a0dd10b350744a3215369db9c40263c11b70985a
sourojyotipaul <sourojyotipaul@gmail.com>
2019-06-22 22:41:36 +0530

    Updated map and slider

--- app/src/main/java/org/tensorflow/lite/examples/classification/StoreFragment.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/StoreFragment.java
@@ -7,12 +7,40 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class StoreFragment extends Fragment {
+
+    private WebView mywebView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_store, container, false);
+        View v = inflater.inflate(R.layout.fragment_store, container, false);
+
+        mywebView = (WebView)v.findViewById(R.id.sWebView);
+        WebSettings webSettings = mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebView.getSettings().setDomStorageEnabled(true);
+        mywebView.getSettings().setDatabaseEnabled(true);
+        mywebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        mywebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        mywebView.getSettings().setAppCacheEnabled(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        //webSettings.setUseWideViewPort(true);
+        webSettings.setSavePassword(true);
+        webSettings.setSaveFormData(true);
+        webSettings.setEnableSmoothTransition(true);
+        mywebView.loadUrl("file:///android_asset/test/slider.html");
+        //In Case of web page error
+        mywebView.setWebViewClient(new WebViewClient(){
+            public void onReceivedError(WebView view, int errorCode, String description, String failingURL) {
+                mywebView.loadUrl("file:///android_asset/error.html");
+            }
+        });
+
+        return v;
     }
 }
